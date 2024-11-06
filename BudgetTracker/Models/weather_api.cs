d6602a2087d3e4fdcee3ef0ef7829fff9951020e
John Boja <crisostomoiboja@gmail.com>
2024-11-06 12:18:02 +0800

    changes on weather api model

--- BudgetTracker/Models/weather_api.cs
+++ BudgetTracker/Models/weather_api.cs
@@ -1,20 +1,12 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-
-namespace BudgetTracker.Models
+﻿namespace BudgetTracker.Models
 {
-    public class Clouds
-    {
-        public int all { get; set; }
-    }
-
-    public class Coord
+    public class coord
     {
         public double lon { get; set; }
         public double lat { get; set; }
     }
-
-    public class Main
+    
+    public class main
     {
         public double temp { get; set; }
         public double feels_like { get; set; }
@@ -26,40 +18,13 @@ namespace BudgetTracker.Models
         public int grnd_level { get; set; }
     }
 
-    public class Rain
+    public class  wind
     {
-        [JsonProperty("1h")]
-        public double _1h { get; set; }
+        public double speed { get; set; }
+        public int deg { get; set; }
     }
 
-    public class Root
-    {
-        public Coord coord { get; set; }
-        public List<Weather> weather { get; set; }
-        public string @base { get; set; }
-        public Main main { get; set; }
-        public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Rain rain { get; set; }
-        public Clouds clouds { get; set; }
-        public int dt { get; set; }
-        public Sys sys { get; set; }
-        public int timezone { get; set; }
-        public int id { get; set; }
-        public string name { get; set; }
-        public int cod { get; set; }
-    }
-
-    public class Sys
-    {
-        public int type { get; set; }
-        public int id { get; set; }
-        public string country { get; set; }
-        public int sunrise { get; set; }
-        public int sunset { get; set; }
-    }
-
-    public class Weather
+    public class weather
     {
         public int id { get; set; }
         public string main { get; set; }
@@ -67,10 +32,12 @@ namespace BudgetTracker.Models
         public string icon { get; set; }
     }
 
-    public class Wind
+    public class sys
     {
-        public double speed { get; set; }
-        public int deg { get; set; }
-        public double gust { get; set; }
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
     }
 }
